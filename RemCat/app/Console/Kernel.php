004a96e22302c6f5f36ad042b05633a6410f906c
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-02-28 14:51:38 +0100

    Command schedule
    No lo vamos a usar pero lo dejo por si interesa algun dia

--- RemCat/app/Console/Kernel.php
+++ RemCat/app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        //$schedule->command("season:create")->yearly()->on("09-01");
     }
 
     /**
