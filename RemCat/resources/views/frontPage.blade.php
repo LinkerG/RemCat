383037cf823c819e7e94334c0173804c0bb756b6
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-03-11 17:33:38 +0100

    footer

--- RemCat/resources/views/frontPage.blade.php
+++ RemCat/resources/views/frontPage.blade.php
@@ -9,36 +9,38 @@
     <title>Document</title>
 </head>
 <body>
-    @include("components.header")
-    <div class="container-fluid bg-dark banner-container">
-        <div class="container over-banner">
-            <img src="{{asset('images/logo-white.png')}}" alt="rem cat logo" class="banner-logo">
-            <div class="textWrapper">
-                <p class="selfWrittingText h1"></p>
+    <div class="wrapper">
+        @include("components.header")
+        <div class="container-fluid bg-dark banner-container">
+            <div class="container over-banner">
+                <img src="{{asset('images/logo-white.png')}}" alt="rem cat logo" class="banner-logo">
+                <div class="textWrapper">
+                    <p class="selfWrittingText h1"></p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container ms-4 mt-3 mb-5">
-        <h2>{{ trans("text.nextCompetition") }}</h2>
-    </div>
-    <section class="container-fluid overflow-auto competitionSection mt-4 mb-4">
-        <div class="d-inline-flex flex-row flex-nowrap overflow-auto competitionContainer">
-            @if(($competitions))
-                @foreach($competitions as $competition)
-                    <article class="flex-shrink-0 competitionItem">
-                        <div class="">
-                            <h4 class="">{{$competition->name}}</h4>
-                            <p class="">{{$competition->location}} - {{$competition->date}}</p>
-                            <a href="#" class="">Card link</a>
-                            <a href="#" class="">Another link</a>
-                        </div>
-                    </article>
-                @endforeach
-            @else
-                <h1>No hay</h1>
-            @endif
+        <div class="container ms-4 mt-3 mb-5">
+            <h2>{{ trans("text.nextCompetition") }}</h2>
         </div>
-    </section>
+        <section class="container-fluid overflow-auto competitionSection mt-4 mb-4">
+            <div class="d-inline-flex flex-row flex-nowrap overflow-auto competitionContainer">
+                @if(($competitions))
+                    @foreach($competitions as $competition)
+                        <article class="flex-shrink-0 competitionItem">
+                            <div class="">
+                                <h4 class="">{{$competition->name}}</h4>
+                                <p class="">{{$competition->location}} - {{$competition->date}}</p>
+                                <a href="#" class="">Card link</a>
+                                <a href="#" class="">Another link</a>
+                            </div>
+                        </article>
+                    @endforeach
+                @else
+                    <h1>No hay</h1>
+                @endif
+            </div>
+        </section>
+    </div>
     @include("components.footer")
 </body>
 </html>
