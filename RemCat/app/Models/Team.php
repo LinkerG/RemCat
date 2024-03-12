81f75c8135dd18d9eb736d082e26513fa3d3a78f
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-03-12 10:17:18 +0100

    modelos en minuscula

--- RemCat/app/Models/Team.php
+++ RemCat/app/Models/Team.php
@@ -8,5 +8,5 @@ use MongoDB\Laravel\Eloquent\Model;
 class Team extends Model
 {
     protected $connection = "mongodb";
-    protected $collection = "Teams";
+    protected $collection = "teams";
 }
