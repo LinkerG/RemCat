641bc44081760ca1f136c43d0275219f6094a766
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-04-01 22:39:26 +0200

    aaa

--- RemCat/resources/views/competitions/viewCompetitions.blade.php
+++ RemCat/resources/views/competitions/viewCompetitions.blade.php
@@ -12,8 +12,15 @@
         @include('components.header')
         <section class=" ">
             @foreach ($competitions as $competition)
-                <article>
-                    <h2>{{ $competition->name }}</h2>
+                <article class="card" style="width: 18rem;">
+                    <?php $bannerRoute = "storage/competition-banners/" . $competition->image_banner; ?>
+                    <img src="{{ asset($bannerRoute) }}" class="card-img-top" alt="{{ $competition->name }} banner">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $competition->name }}</h5>
+                        <p class="card-text"></p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="#" class="btn btn-primary">Go somewhere else</a>
+                    </div>
                 </article>
             @endforeach
         </section>
