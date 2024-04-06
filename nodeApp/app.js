e16e877baeff230b0efd03de57795ca67845f64d
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-04-06 19:20:34 +0200

    React + vistas en ruta /

--- nodeApp/app.js
+++ nodeApp/app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('start', { title: 'Página de inicio' });
+});
+
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.status(404).send("Lo siento, no puedo encontrar esa página.");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Algo salió mal!');
+});
 
 app.listen(port, (err) => {  
   if (err) {
-    return console.log('something bad happened', err);
+    return console.log('¡Algo malo sucedió!', err);
   }
 
-  console.log(`server is listening on ${port}`);
+  console.log(`¡El servidor está escuchando en el puerto ${port}!`);
 });
---
+++ nodeApp/views/start.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+const Start = ({ title }) => (
+    <html>
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+        <h1>{title}</h1>
+        <p>Bienvenido a mi aplicación Express con React</p>
+    </body>
+    </html>
+);
+
+module.exports = Start;
