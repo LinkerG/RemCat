be739d222ca382f381704dd39a37e61ec449366e
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-04-09 12:58:49 +0200

    ws 3

--- RemCat/resources/views/shareLocation.blade.php
+++ RemCat/resources/views/shareLocation.blade.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="{{ asset('js/websocketManager.js') }}"></script>
+    <title>Pusher Test</title>
+    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+    <script>
+        // Enable pusher logging - don't include this in production
+        Pusher.logToConsole = true;
+
+        var pusher = new Pusher('afc6c0625d1fea458a18', {
+            cluster: 'eu'
+        });
+
+        var channel = pusher.subscribe('my-channel');
+        channel.bind('my-event', function(data) {
+            alert(JSON.stringify(data));
+        });
+    </script>
 </head>
 <body>
+    <h1>Pusher Test</h1>
+    <p>
+        Try publishing an event to channel <code>my-channel</code>
+        with event name <code>my-event</code>.
+    </p>
     <h1>{{ $year }}</h1>
     <h1>{{ $competition_id }}</h1>
     <h1>{{ $result_id }}</h1>
